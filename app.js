c912620505ded6212b5a3d7731c12f8f23b8557b
Devon Hogencamp <devon.hogencamp@west-mec.org>
2016-12-15 15:26:06 -0700

    the app and finished routes

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-var app = require('express');
-var router = require('express-router');
-var 
+/*  Main app for to do products app  */
+
+// Require all neccesary packages and modules
+var express = require('express');
+var products = require('./routes/products');
+
+// Start the express app
+var app = express();
+
+// Fire the products router
+products(app);
+
+// Listen for the 3000 port
+app.listen(3000);
+
+// Let us know which port the server is running on
+console.log('Server running on port 3000');
--- routes/products.js
+++ routes/products.js
@@ -11,10 +11,61 @@ mongoose.connect('mongodb://localhost:27017/products');
 var productsSchema = new mongoose.Schema({
     brand: String,
     model: String,
-    megapixels: [Number],
-    rating: [Number],
-    price: [Number]
+    megapixels: Number,
+    rating: Number,
+    price: Number
 });
 
 // Way to connect to mongoose
 var Products = mongoose.model('Products', productsSchema);
+
+// Export this for the app.js to call
+module.exports = function (app) {
+    // When they get the products
+    app.get('/products', function (req, res) {
+        // Find all items from the database
+        Products.find({}, function (err, data) {
+            if (err) {
+                throw err;
+            }
+            res.json(data);
+        });
+    });
+
+    // When they get a specific product
+    app.get('/products/:id', function (req, res) {
+        // Find the item with that id in the database
+        Products.find({_id: req.params.id}, function (err, data) {
+            if (err) {
+                throw err;
+            }
+            res.json(data);
+        });
+    });
+
+    // When they create a product
+    app.post('/products', function (req, res) {
+        // Get data from the view and add it to mongodb
+        var newProducts = Products(req.body).save(function(err, data) {
+            if (err) {
+                throw err;
+            }
+            res.json(data);
+        });
+    });
+
+    // When they update a product
+    app.put('/products', function (req, res) {
+
+    });
+
+    // When they delete a product
+    app.delete('/products', function (req, res) {
+        Products.find({_id: req.params.id}).remove(function (req, res) {
+            if (err) {
+                throw err;
+            }
+            res.json(data);
+        });
+    });
+};
